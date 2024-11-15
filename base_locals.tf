39e8f7ff793f01e2a90aa6892c4b009d4985387f
吉村直人 <ahowateto123@gmail.com>
2024-11-15 22:40:54 +0900

    root: base locals

--- base_locals.tf
+++ base_locals.tf
@@ -4,10 +4,14 @@ locals {
   tf_s3_bucket    = "fargate-example-terraform"
   root_state_file = "terraform.tfstate"
   base_domain     = "fargate-example.com"
+  repository      = "yuji-sniper/fargate-example-infrastructure"
   default_tags = {
     Managed     = "terraform"
     Project     = local.project
     Environment = local.env
     Repository  = local.repository
   }
+  ips = [
+    # IP addresses
+  ]
 }
